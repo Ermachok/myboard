ac2ed25cbd2c17c3af7b355f0a7ab3b79d95c3ad
NikitaErmakov <ermafin@gmail.com>
2025-05-25 17:47:36 +0300

    added board and task models to alembic

--- alembic/env.py
+++ alembic/env.py
@@ -9,9 +9,9 @@ from alembic import context
 load_dotenv()
 
 from app.db.database import Base
-from app.models.user import User
 from app.models.board import Board
 from app.models.task import Task
+from app.models.user import User
 
 config = context.config
 
